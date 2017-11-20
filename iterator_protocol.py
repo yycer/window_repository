8706e6314d65b13bac115c336e3fca474bad2fa3
yyc <15900617310@163.com>
2017-11-20 13:52:12 +0800

    iterator protocol

---
+++ iterator_protocol.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 13:46:27 2017
+
+"""
+
+def isiterable(obj):
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+
+print(isiterable('a string'))       # True
+print(isiterable([1, 2, 3]))        # True
+print(isiterable((1, 2, 3)))        # True
+print(isiterable({'name': 'Gerg'})) # True
+print(isiterable({'name', 'age'}))  # True
