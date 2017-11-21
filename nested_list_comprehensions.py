c6905b5d76b43aa51af99d01fcb5740d0704e264
yycer <15900617310@163.com>
2017-11-21 14:17:29 +0800

    Update nested_list_comprehensions.py

--- nested_list_comprehensions.py
+++ nested_list_comprehensions.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 21 14:14:25 2017
-
-@author: Administrator
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Nov 21 11:08:10 2017
 
 @author: yyc
