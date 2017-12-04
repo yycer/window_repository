4568745f0207c44dc843140a38dbf8dbe197290c
yyc <15900617310@163.com>
2017-12-04 17:01:08 +0800

    math.log(x, base)

---
+++ Data_analysis/math_log.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 16:58:31 2017
+
+@author: log
+"""
+from math import log
+
+
+# log(x[, base]) - returns the logarithm of x to the given base.
+print(log(100, 10)) # 2.0
+print(log(8, 2))    # 3.0
