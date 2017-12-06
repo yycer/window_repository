9a3958183037cc9a4d258913cc7e6598f723f29e
yyc <15900617310@163.com>
2017-12-06 19:48:27 +0800

    list.index()

---
+++ Data_analysis/list_index().py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  6 19:46:56 2017
+
+@author: yyc
+"""
+
+l = ['Gerg', 'asan', 'pangzi']
+print(l.index('Gerg'))      # 0
+print(l.index('asan'))      # 1
+print(l.index('pangzi'))    # 2
