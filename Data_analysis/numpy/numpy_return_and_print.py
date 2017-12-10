799369ccb4572237d748cc8dd5df09450a9d4bdc
yyc <15900617310@163.com>
2017-12-10 14:14:23 +0800

    combination return with print

---
+++ Data_analysis/numpy/numpy_return_and_print.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 14:10:50 2017
+
+@author: yyc
+"""
+
+import numpy as np
+
+a = np.arange(-2, 2)     # [-2 -1  0  1]
+print(10 * np.sin(a))    # [-9.09297427 -8.41470985  0.          8.41470985]
+
+def test(data):
+    result = 10 * np.sin(data)
+    return print(result < 0)
+
+test(a)                  # [ True  True False False]
