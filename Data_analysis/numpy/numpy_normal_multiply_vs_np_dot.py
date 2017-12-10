c74edeca98dbab2ca1f244265a9954a62ded52f9
yyc <15900617310@163.com>
2017-12-10 14:51:40 +0800

    numpy noraml multiply vs np.dot

---
+++ Data_analysis/numpy/numpy_normal_multiply_vs_np_dot.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 14:47:37 2017
+
+@author: yyc
+"""
+
+import numpy as np
+
+a = np.array([10, 20, 30, 40])
+b = np.arange(4)
+
+a = a.reshape((2, 2))
+b = b.reshape((2, 2))
+
+normal_multiply = a * b
+print(normal_multiply)      # [[  0  20]
+#                              [ 60 120]]
+
+np_dot = np.dot(a, b)
+print(np_dot)               # [[ 40  70]
+                            #  [ 80 150]]
