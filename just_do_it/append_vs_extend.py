afe6895547856c860af67e112debb0d014ee6a29
yyc <15900617310@163.com>
2017-12-11 08:49:20 +0800

    More append & extend

--- just_do_it/append_vs_extend.py
+++ just_do_it/append_vs_extend.py
@@ -10,9 +10,14 @@ l2 = [True, False]
 
 l1_copy = [1, 2]
 l2_copy = [True, False]
-
+l = ['asan', 'Gerg', 'pangzi']
+# append: adds an element to a list.
 l1.append(l2)
 print(l1)                   # [1, 2, [True, False]]
 
+# extend: concatenates the first list with another list(or another iterable)
 l1_copy.extend(l2_copy)
 print(l1_copy)              # [1, 2, True, False]
+
+l.extend('string')
+print(l)                    # ['asan', 'Gerg', 'pangzi', 's', 't', 'r', 'i', 'n', 'g']
