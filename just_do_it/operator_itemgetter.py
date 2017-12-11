7b787ec130700fade5da92614b57dd0c9b1b43ab
yyc <15900617310@163.com>
2017-12-11 16:36:10 +0800

    sorted(iterable, key = operator.itemgetter(indices))

--- just_do_it/operator_itemgetter.py
+++ just_do_it/operator_itemgetter.py
@@ -6,6 +6,7 @@ Created on Sat Dec  2 20:31:50 2017
 """
 import operator
 
+# sorted(key = operator.itemgetter(indices))  对于迭代对象中的元素的属性进行排序
 students = [('Gerg', 'C', 21), ('asan', 'A', 22), ('Qin', 'B', 23)]
 # [('asan', 'A', 22), ('Qin', 'B', 23), ('Gerg', 'C', 21)]
 print(sorted(students, key = operator.itemgetter(1)))
