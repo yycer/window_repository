64ba088b807147ffc0e8b0f422f00e6117b93593
yyc <15900617310@163.com>
2017-12-11 17:11:20 +0800

    adjust

--- Data_analysis/numpy/numpy_return_and_print.py
+++ Data_analysis/numpy/numpy_return_and_print.py
@@ -8,8 +8,8 @@ Created on Sun Dec 10 14:10:50 2017
 import numpy as np
 
 a = np.arange(-2, 2)     # [-2 -1  0  1]
-b = np.arange(10)        # [False False False False False False  True  True  True  True]
-print(b > 5)
+b = np.arange(10)        
+print(b > 5)             # [False False False False False False  True  True  True  True]
 
 print(10 * np.sin(a))    # [-9.09297427 -8.41470985  0.          8.41470985]
 
