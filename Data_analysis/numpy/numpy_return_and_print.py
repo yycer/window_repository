cbf800f670475e2bbcbbf7a36e1fbfa854f03911
yyc <15900617310@163.com>
2017-12-11 17:01:19 +0800

    print(condition) with np.array

--- Data_analysis/numpy/numpy_return_and_print.py
+++ Data_analysis/numpy/numpy_return_and_print.py
@@ -8,6 +8,9 @@ Created on Sun Dec 10 14:10:50 2017
 import numpy as np
 
 a = np.arange(-2, 2)     # [-2 -1  0  1]
+b = np.arange(10)        # [False False False False False False  True  True  True  True]
+print(b > 5)
+
 print(10 * np.sin(a))    # [-9.09297427 -8.41470985  0.          8.41470985]
 
 def test(data):
