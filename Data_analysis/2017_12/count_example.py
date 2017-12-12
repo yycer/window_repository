20fb383f4dfae77d50ef61d5ea2836b2eac5d361
yyc <15900617310@163.com>
2017-12-12 17:56:16 +0800

    list.count(1) example

---
+++ Data_analysis/2017_12/count_example.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 17:54:15 2017
+
+@author: yyc
+"""
+
+# naive bayes: myVocabList
+# len(l)  32
+l = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0]
+print(l.count(1)) # 7
+print(l.count(0)) # 25
