f823c3af145e4f8982a253b07fb5a819a5eb2e5e
yyc <15900617310@163.com>
2017-12-25 10:27:47 +0800

    向整数相除输出小数部分

---
+++ just_do_it/2017_12/__future__division.py
@@ -0,0 +1,11 @@
+# naive:
+# print 10 / 3            # 3
+
+
+# first way:
+# from __future__ import division
+# print 10 / 3            # 3.33333333333
+
+
+# second way:
+# print float(10) / 3     # 3.33333333333
