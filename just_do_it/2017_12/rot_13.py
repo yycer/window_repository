37ef05f26da5cb18521d563af6a21ca03bf018e4
yyc <15900617310@163.com>
2017-12-25 16:12:19 +0800

    rot_13 yyc ---> llp

--- just_do_it/2017_12/rot_13.py
+++ just_do_it/2017_12/rot_13.py
@@ -1,6 +1,13 @@
 # worked only in python 2
-
+from string import  ascii_lowercase, ascii_uppercase, maketrans
 
 a = 'Hello world'
 
-print(a.encode('rot_13'))   # Uryyb jbeyq
+print a.encode('rot_13')                          # Uryyb jbeyq
+
+
+rot13_lowercase = ascii_lowercase[13: ] + ascii_lowercase[: 13]
+rot13_uppercase = ascii_uppercase[13: ] + ascii_uppercase[: 13]
+rot13_table = maketrans(ascii_lowercase + ascii_uppercase, rot13_lowercase + rot13_uppercase)
+
+print 'Hello world'.translate(rot13_table)        # Uryyb jbeyq
