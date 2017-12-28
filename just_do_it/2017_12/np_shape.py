1ef15eb1fe225e93a8098e60e3c9926f6874f325
yyc <15900617310@163.com>
2017-12-28 15:33:23 +0800

    (9, ) to (3, 3)  shape

---
+++ just_do_it/2017_12/np_shape.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 28 15:31:38 2017
+
+@author: yyc
+"""
+
+import numpy as np
+
+m1 = np.arange(9)       # [0 1 2 3 4 5 6 7 8]
+
+m1.shape = 3, 3
+print(m1)
+#    [[0 1 2]
+#     [3 4 5]
+#     [6 7 8]]
