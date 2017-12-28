4c78e0afa062db410ac280f39470211055d641cc
yyc <15900617310@163.com>
2017-12-28 16:02:19 +0800

    用reshape替代shape, 千丝万缕， 重新复制给一个变量即可

--- deep_learning/np_shape.py
+++ deep_learning/np_shape.py
@@ -7,10 +7,13 @@ Created on Thu Dec 28 15:31:38 2017
 
 import numpy as np
 
-m1 = np.arange(9)       # [0 1 2 3 4 5 6 7 8]
+#m1 = np.arange(9)       # [0 1 2 3 4 5 6 7 8]
 
-m1.shape = 3, 3
-print(m1)
+#m1.shape = 3, 3
+m1_to_33 = np.reshape(np.arange(9), (3, 3))
+
+print(m1_to_33)
+#print(m1)
 #    [[0 1 2]
 #     [3 4 5]
 #     [6 7 8]]
