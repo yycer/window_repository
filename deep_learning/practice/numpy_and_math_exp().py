f1124cfdaf2f6cc4bce2816a1330b4166e7d4002
yyc <15900617310@163.com>
2018-01-01 11:40:20 +0800

    np.exp() vs math.exp()

---
+++ deep_learning/practice/numpy_and_math_exp().py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+
+l1 = range(5)
+print l1                # [0, 1, 2, 3, 4]
+
+# numpy.exp() - Calculate the exponential of all elements in the input array.
+print np.exp(l1)        # [  1.           2.71828183   7.3890561   20.08553692  54.59815003]
+
+print math.exp(2)       # 7.38905609893
+print math.exp(3)       # 20.0855369232
+print math.exp(4)       # 54.5981500331
