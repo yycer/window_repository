9b8cf1c9ee65456767d09b130cbe01670f6d625e
yyc <15900617310@163.com>
2018-02-21 16:46:24 +0800

    re.match() vs re.search()区别

--- just_do_it/re/match_search_findall.py
+++ just_do_it/re/match_search_findall.py
@@ -4,7 +4,7 @@ Created on Tue Dec 19 10:36:50 2017
 
 @author: yyc
 """
-
+# 参考：http://www.pythontab.com/html/2013/pythonjichu_0201/199.html
 import re
 
 a = 'spartacus purse the freedo\nm'
@@ -15,16 +15,25 @@ print(a)                            # spartacus purse the freedo
 print(b)                            # spartacus purse the freedo\nm
 
 c = 'spartacus purse the freedom spartacus'
+d = '@spartacus purse the freedom spartacus'
+
 # re.match(pattern, string, flag = 0)
 # Try to apply the pattern at the start of the string,
 # returning a match object, or None if no match was found.
+# match()只有当起始位置匹配成功，才有返回，否则为None
 match1 = re.match(r'\w+', c)        # \w: a-zA-Z0-9_
+match11 = re.match(r'\w+', d)
 print(match1.group(0))              # spartacus
+if match11:
+    print(match11.group(0))
+else:
+    print('match11 is not match')   # match11 is not match
 
 
 # re.search(pattern, string, flag = 0)
 # Scan through string looking for a match to the pattern, returning
 # a match object, or None if no match was found.
+# search()会扫描整个字符串并返回第一个成功的匹配
 match2 = re.search('spartacus', c)
 print(match2.group(0))              # spartacus
 print(match2.start())               # 0
