0fb00a8047d7251add37797e9c7922e932a703f8
yycer <15900617310@163.com>
2019-05-15 08:39:25 +0800

    Update insertion sort

--- Projects/Java/sort-algorithm-demo/src/test/java/com/frankie/demo/DemoApplicationTests.java
+++ Projects/Java/sort-algorithm-demo/src/test/java/com/frankie/demo/DemoApplicationTests.java
@@ -31,4 +31,37 @@ public class DemoApplicationTests {
         }
     }
 
+
+    @Test
+    public void selectionLargestSort(){
+
+        int[] ints = {8, 5, 7, 3, 9, 6};
+
+        for (int i = 0; i < ints.length - 1; i++){
+            int max = i;
+            for (int j = i + 1; j < ints.length; j++){
+                if(ints[j] > ints[max]) max = j;
+            }
+            if(ints[i] != ints[max]){
+                int tmp = ints[i];
+                ints[i] = ints[max];
+                ints[max] = tmp;
+            }
+        }
+    }
+
+    @Test
+    public void insertionSort(){
+
+        int[] ints = {8, 5, 7, 3, 9, 6};
+        for (int i = 1; i < ints.length; i++){
+            for (int j = i - 1; j >= 0; j--){
+                if(ints[i] < ints[j]){
+                    int tmp = ints[j];
+                    ints[j] = ints[i];
+                    ints[i] = tmp;
+                }
+            }
+        }
+    }
 }
