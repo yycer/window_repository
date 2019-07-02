793bf9ab306a44de66d5d568b8dcb7ad78034695
yycer <15900617310@163.com>
2019-07-02 21:46:55 +0800

    Tidy up arrayDeque methods.

--- Projects/Java/jian-zhi-offer/src/main/java/com/frankie/demo/ArrayDequeUtils.java
+++ Projects/Java/jian-zhi-offer/src/main/java/com/frankie/demo/ArrayDequeUtils.java
@@ -20,6 +20,21 @@ public class ArrayDequeUtils {
      */
 
 
-    // region ArrayDeque(双端队列)
-    // endregion
+    /**
+     * ArrayDeque
+     * 一. Stack
+     * 1. push()        - 也有这个方法，但本质是addFirst()
+     * 2. pop()         - 也有这个方法，但本质是removeFirst()
+     * 3. peek()        - 也有这个方法，但本质是peekFirst()
+     *
+     * 二. Queue
+     * 1. add()         - 也有这个方法，但本质是addLast()
+     * 2. offer()       - 也有这个方法，但本质是offerLast() -> addLast()
+     * 3. remove()      - 也有这个方法，但本质是removeFirst()
+     * 4. poll()        - 也有这个方法，但本质是pollFirst()
+     * 5. element()     - 也有这个方法，但本质是getFirst()
+     * 6. peek()        - 也有这个方法，但本质是peekFirst()
+     *
+     */
+
 }
