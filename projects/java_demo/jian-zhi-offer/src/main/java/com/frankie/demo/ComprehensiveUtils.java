823870cf6da0504a0e1b8198feeb90834c509dc1
yycer <15900617310@163.com>
2019-08-10 11:21:23 +0800

    字符串的排列、组合

--- projects/java_demo/jian-zhi-offer/src/main/java/com/frankie/demo/ComprehensiveUtils.java
+++ projects/java_demo/jian-zhi-offer/src/main/java/com/frankie/demo/ComprehensiveUtils.java
@@ -393,52 +393,6 @@ public class ComprehensiveUtils {
     }
 
     /**
-     * 组合算法：[abc]的组合方式有[a,b,c,ab,bc,ac,abc]六种组合方式
-     */
-    public List<String> combination(char[] buf){
-        ArrayList<String> list = new ArrayList<>();
-        // 1 << buf.length，即2的buf.length次方。
-        for(int i = 1 ; i < (1 << buf.length); i++){
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0 ; j < buf.length; j++){
-                //判断哪一位为1
-                if((i & (1 << j)) != 0){
-                    sb.append(buf[j]);
-                }
-            }
-            list.add(new String(sb));
-        }
-        return list;
-    }
-
-    private ArrayList<String> permutationList = new ArrayList<>();
-    /**
-     * 字符串的排列。
-     * 1. 递归结束条件: start等于end，拼buf，加入list即可。
-     * 2. 否则调整数组、递归(start+1)、还原数组。
-     */
-    public List<String> permutation(char[] buf, int start, int end){
-
-        if (start == end){
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < end; i++){
-               sb.append(buf[i]);
-            }
-            permutationList.add(new String(sb));
-            sb = new StringBuilder();
-        } else {
-            for (int i = start; i < buf.length; i++){
-                // 排列
-                ArrayUtils.swap(buf, i, start);
-                permutation(buf, start + 1, end);
-                // 还原
-                ArrayUtils.swap(buf, i, start);
-            }
-        }
-        return permutationList;
-    }
-
-    /**
      * 连续子数组的最大和(动态规划)。
      */
     public int findGreatestSumOfSubArrayUsingDP(int[] a){
@@ -1163,6 +1117,54 @@ public class ComprehensiveUtils {
 
         return count;
     }
+    // endregion
+
+    // region String
+    /**
+     * 字符串的排列。
+     * 1. 递归结束条件: start等于end，拼buf，加入list即可。
+     * 2. 否则调整数组、递归(start+1)、还原数组。
+     */
+    private ArrayList<String> permutationList = new ArrayList<>();
+    public List<String> permutation(char[] buf, int start, int end){
+
+        if (start == end){
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < end; i++){
+                sb.append(buf[i]);
+            }
+            permutationList.add(new String(sb));
+        } else {
+            for (int i = start; i < buf.length; i++){
+                // 排列
+                ArrayUtils.swap(buf, i, start);
+                permutation(buf, start + 1, end);
+                // 还原
+                ArrayUtils.swap(buf, i, start);
+            }
+        }
+        return permutationList;
+    }
+
+    /**
+     * 组合算法：[abc]的组合方式有[a,b,c,ab,bc,ac,abc]六种组合方式
+     */
+    public List<String> combination(char[] buf){
+        ArrayList<String> list = new ArrayList<>();
+        // 1 << buf.length，即2的buf.length次方。
+        for(int i = 1 ; i < (1 << buf.length); i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0 ; j < buf.length; j++){
+                //判断哪一位为1
+                if((i & (1 << j)) != 0){
+                    sb.append(buf[j]);
+                }
+            }
+            list.add(new String(sb));
+        }
+        return list;
+    }
+
     // endregion
 }
 
