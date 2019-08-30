1f8bc04bc88feeb05309678a033a3977cb5f2785
yaofrankie <15900617310@163.com>
2019-08-30 16:18:33 +0800

    Update DvdPlayer file name

--- projects/java_demo/spring-playground/src/main/java/com/frankie/demo/soundsystem/DVDPlayer.java
+++ projects/java_demo/spring-playground/src/main/java/com/frankie/demo/soundsystem/DVDPlayer.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-/**
- * @author Administrator
- */
+
 @Component
 public class DvdPlayer implements MediaPlayer {
 
